54356793a51aba210a3948ba65966468df358d18
ajmalgithubm <ajmalm76774@gmail.com>
2023-10-28 14:52:20 +0530

    navbra completed

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const searchBtn = document.querySelector('#search-btn');
 const searchBar = document.querySelector('.search-bar-container');
 
+const menuBar = document.querySelector('#menu-bar')
+
 const loginBtn = document.querySelector('#login-user');
 const loginForm = document.querySelector('.login-form-container');
-const cancelBtn = document.querySelector('#form-close')
+const cancelBtn = document.querySelector('#form-close');
+
+const navbar = document.querySelector('.navbar')
 
 searchBtn.addEventListener('click', () => {
     console.log("btn clicked")
     searchBtn.classList.toggle('fa-times')
     searchBar.classList.toggle('active');
+})
+
+menuBar.addEventListener('click', () => {
+    navbar.classList.toggle('active-drop-down');
+    menuBar.classList.toggle('fa-times')
 })
 
 cancelBtn.addEventListener('click', () => {
@@ -22,4 +31,6 @@ loginBtn.addEventListener('click', () => {
 window.onscroll = () => {
     searchBtn.classList.remove('fa-times')
     searchBar.classList.remove('active');
+    searchBar.classList.remove('active-drop-down');
+    menuBar.classList.remove('fa-times')
 }
