9db361dee9cd74f0b2c080de123f08a39acc01f1
ajmalgithubm <ajmalm76774@gmail.com>
2023-10-28 14:30:39 +0530

    login form created

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const searchBtn = document.querySelector('#search-btn');
 const searchBar = document.querySelector('.search-bar-container');
 
+const loginBtn = document.querySelector('#login-user');
+const loginForm = document.querySelector('.login-form-container');
+const cancelBtn = document.querySelector('#form-close')
+
 searchBtn.addEventListener('click', () => {
     console.log("btn clicked")
     searchBtn.classList.toggle('fa-times')
     searchBar.classList.toggle('active');
+})
+
+cancelBtn.addEventListener('click', () => {
+    loginForm.classList.remove('active')
+})
+
+loginBtn.addEventListener('click', () => {
+    loginForm.classList.add('active')
 })
 
 window.onscroll = () => {
